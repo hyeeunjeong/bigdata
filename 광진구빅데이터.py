128a7e3ff1217fbe53a1131f4ea7a69a7c570f50
esphi181 <shepi5000@naver.com>
2024-03-10 16:38:48 +0900

    240310 : waste plot

--- 광진구빅데이터.py
+++ 광진구빅데이터.py
@@ -1,23 +1,22 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-domestic_df=pd.read_csv('../서울특별시 광진구_가정폐기물현황_20240131.csv', encoding='cp949')
-months=[]
-recycles=[]
-foods=[]
+df=pd.read_csv('../서울특별시 광진구_가정폐기물현황_20240131.csv', encoding='cp949')
+new_df=pd.DataFrame({"year":(pd.to_datetime(df["발생년월"])).dt.strftime("%Y"), 
+                     "domestic":df[" 재활용폐기물(톤) "],
+                     "food":df[" 음식물폐기물(톤) "]})
+print(new_df)
 
-for line in domestic_df:
-    (month, recycle, food) = line.split(',')
-    months.append(str(month))       
-    recycles.append(int(recycle))
-    foods.append(int(food))
+new_df.groupby("year")['domestic'].sum()
 
+df1 = new_df.groupby("year")['domestic','food'].sum()
+print(df1)
 
 plot_domestics = plt.figure()
 ax = plot_domestics.add_subplot(1,1,1)
 
-# plt.plot(domestic_df["발생년월"], domestic_df[" 재활용폐기물(톤) "], color='green', marker='o', linestyle='solid')
-# plt.plot(domestic_df["발생년월"], domestic_df[" 음식물폐기물(톤) "], color='red', marker='o', linestyle='solid')
+plt.plot(df1['domestic'], color='green', marker='o', linestyle='solid')
+plt.plot(df1['food'], color='red', marker='o', linestyle='solid')
 
 plt.title("Gwangjin Domestic Waste")
 plt.show()

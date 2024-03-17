53a4b61a4fa54e246ed41ad03fc8485c9c1c1691
esphi181 <shepi5000@naver.com>
2024-03-17 22:38:47 +0900

    240317 update

--- Domestic_Food.py
+++ Domestic_Food.py
@@ -1,6 +1,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+import matplotlib
+matplotlib.rcParams['font.family'] = 'Malgun Gothic'
+matplotlib.rcParams['font.size'] = 10
+matplotlib.rcParams['axes.unicode_minus'] = False
+
 df=pd.read_csv('../서울특별시 광진구_가정폐기물현황_20240131.csv', encoding='cp949')
 new_df=pd.DataFrame({"year":(pd.to_datetime(df["발생년월"])).dt.strftime("%Y"), 
                      "domestic":df[" 재활용폐기물(톤) "],
@@ -15,8 +20,12 @@ print(df1)
 plot_domestics = plt.figure()
 ax = plot_domestics.add_subplot(1,1,1)
 
-plt.plot(df1['domestic'], color='green', marker='o', linestyle='solid')
-plt.plot(df1['food'], color='red', marker='o', linestyle='solid')
+plt.plot(df1['domestic'], color='green', marker='o', linestyle='solid', label='가정폐기물')
+plt.plot(df1['food'], color='red', marker='o', linestyle='solid',label='음식폐기물')
+plt.legend()
 
-plt.title("Gwangjin Domestic Waste")
+plt.title("광진구 가정 및 음식 폐기물 현황")
 plt.show()
+
+
+
